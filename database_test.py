9469c8df8232f19e5a2535a21d9a0603f2a19401
aaochoa <oe.anderson@cerezostudio.com>
2020-03-24 20:33:30 -0500

    A bunch of files deleted and start shapping a threaded TCP socket server

--- database_test.py
+++ database_test.py
@@ -5,20 +5,26 @@ try:
                                 host = "209.240.107.6",
                                 port = "5432",
                                 database = "si18data_production")
-
   cursor = connection.cursor()
-
-  select_messages_query = 'SELECT id, operator_id FROM operations ORDER BY id ASC LIMIT 5000'
-  
-  cursor.execute(select_messages_query)
+  cursor.execute('SELECT id, operator_id FROM operations ORDER BY id ASC LIMIT 5')
   x = cursor.fetchall()
   print(f"Query in PostgreSQL: {x}")
+  cursor.execute("SELECT version();")
+  record = cursor.fetchone()
+  print("You are connected to - ", record,"\n")
 
 except (Exception, psycopg2.DatabaseError) as error :
-    print ("Error while creating PostgreSQL table", error)
+    print ("Error while conecting PostgreSQL table", error)
 
 finally:
   if(connection):
     cursor.close()
     connection.close()
     print("PostgreSQL connection is closed")
+
+'''
+create_table_query = ''/'CREATE TABLE mobile
+          (ID INT PRIMARY KEY     NOT NULL,
+          MODEL           TEXT    NOT NULL,
+          PRICE         REAL); ''/'
+'''
--- server_threaded.py
+++ server_threaded.py
@@ -1,10 +1,15 @@
 import socket
 import argparse
-import threading 
+import threading
+import logging
+import yaml 
+import psycopg2
+
+conf = yaml.load(open('application.yml'))
 
 parser = argparse.ArgumentParser(description = "This is the server for the multithreaded socket demo!")
 parser.add_argument('--host', metavar = 'host', type = str, nargs = '?', default = socket.gethostname())
-parser.add_argument('--port', metavar = 'port', type = int, nargs = '?', default = 9999)
+parser.add_argument('--port', metavar = 'port', type = int, nargs = '?', default = 8443)
 args = parser.parse_args()
 
 print(f"Running the server on: {args.host} and port: {args.port}")
@@ -15,23 +20,40 @@ sck.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 try: 
 	sck.bind((args.host, args.port))
 	sck.listen(5)
+	connection = psycopg2.connect(user = conf['db']['user'],
+                                password = conf['db']['password'],
+                                host = conf['db']['host'],
+                                port = conf['db']['port'],
+                                database = conf['db']['database'])
+  cursor = connection.cursor()
+	'''cursor.execute('SELECT id, operator_id FROM operations ORDER BY id ASC LIMIT 5')
+  x = cursor.fetchall()
+  print(f"Query in PostgreSQL: {x}")
+  cursor.execute("SELECT version();")
+  record = cursor.fetchone()
+	if(connection):
+    cursor.close()
+    connection.close()
+    print("PostgreSQL connection is closed")'''
 except Exception as e:
 	raise SystemExit(f"We could not bind the server on host: {args.host} to port: {args.port}, because: {e}")
 
-
 def on_new_client(client, connection):
 	ip = connection[0]
-	port = connection[1]
-	print(f"The new connection was made from IP: {ip}, and port: {port}!")
+	port = connection[1]	
 	while True:
 		msg = client.recv(1024)
-		if msg.decode() == 'exit':
-			break
-		print(f"The client said: {msg.decode()}")
-		reply = f"You told me: {msg.decode()}"
-		client.sendall(reply.encode('utf-8'))
-	print(f"The client from ip: {ip}, and port: {port}, has gracefully disconnected!")
+		handle_message(msg)
 	client.close()
+
+def handle_message(messasge):
+	if messasge.decode() == 'exit':
+		break
+	logging.info(f"The new connection was made from IP: {ip}, and port: {port}!")
+	logging.debug(messasge.decode())
+	logging.info(f"The client from ip: {ip}, and port: {port}, has gracefully disconnected!")
+	#reply = "OK"
+	#client.sendall(reply.encode('utf-8'))
 
 while True:
 	try: 
