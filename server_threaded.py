73fbba6a94b886c88f7bc35fd10477cb7268c549
aaochoa <oe.anderson@cerezostudio.com>
2020-03-25 20:32:13 -0500

    Unused file and fixing database connection

--- server_threaded.py
+++ server_threaded.py
@@ -5,45 +5,45 @@ import logging
 import yaml 
 import psycopg2
 
-conf = yaml.load(open('application.yml'))
+conf = yaml.load(open('application.yml'), Loader=yaml.BaseLoader)
 
 parser = argparse.ArgumentParser(description = "This is the server for the multithreaded socket demo!")
 parser.add_argument('--host', metavar = 'host', type = str, nargs = '?', default = socket.gethostname())
 parser.add_argument('--port', metavar = 'port', type = int, nargs = '?', default = 8443)
 args = parser.parse_args()
 
-print(f"Running the server on: {args.host} and port: {args.port}")
+print("Running the server on: {} and port: {}".format(args.host, args.port))
 
 sck = socket.socket()
 sck.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-try: 
-	sck.bind((args.host, args.port))
-	sck.listen(5)
-	'''connection = psycopg2.connect(user = conf['db']['user'], password = conf['db']['password'], host = conf['db']['host'], port = conf['db']['port'], database = conf['db']['database'])
+def get_vehicles():
+	connection = psycopg2.connect(user = conf['db']['user'], password = conf['db']['password'], host = conf['db']['host'], port = conf['db']['port'], database = conf['db']['database'])
 	cursor = connection.cursor()
-	cursor.execute('SELECT id, operator_id FROM operations ORDER BY id ASC LIMIT 10')
+	cursor.execute('SELECT id, internal_code FROM vehicles ORDER BY id ASC')
 	x = cursor.fetchall()
-	print(f"Query in PostgreSQL: {x}")
 	if(connection):
 		cursor.close()
 		connection.close()
-		print("PostgreSQL connection is closed")'''
-except Exception as e:
-	raise SystemExit(f"We could not bind the server on host: {args.host} to port: {args.port}, because: {e}")
+	return x
 
-def on_new_client(client, connection):
-	ip = connection[0]
-	port = connection[1]	
+try: 
+	sck.bind((args.host, args.port))
+	sck.listen(5)
+	records = get_vehicles()
+except Exception as e:
+	raise SystemExit("We could not bind the server on host: {} to port: {}, because: {}".format(args.host, args.port, e))
+
+def on_new_client(client, connection):	
 	while True:
 		msg = client.recv(1024)
 		handle_message(msg)
 	client.close()
 
 def handle_message(messasge):
-	logging.info(f"The new connection was made from IP: {ip}, and port: {port}!")
+	logging.info("The new connection was made from IP: {}, and port: {}!".format(ip, port))
 	logging.debug(messasge.decode())
-	logging.info(f"The client from ip: {ip}, and port: {port}, has gracefully disconnected!")
+	logging.info("The client from ip: {}, and port: {}, has gracefully disconnected!".format(ip, port))
 	#reply = "OK"
 	#client.sendall(reply.encode('utf-8'))
 
@@ -52,8 +52,8 @@ while True:
 		client, ip = sck.accept()
 		threading._start_new_thread(on_new_client,(client, ip))
 	except KeyboardInterrupt:
-		print(f"Gracefully shutting down the server!")
+		print("Gracefully shutting down the server!")
 	except Exception as e:
-		print(f"Well I did not anticipate this: {e}")
+		print("Well I did not anticipate this: {}".format(e))
 
 sck.close()
