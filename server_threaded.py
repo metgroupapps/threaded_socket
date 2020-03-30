86c9b82513742af45fc08636b5505d510d911102
aaochoa <oe.anderson@cerezostudio.com>
2020-03-30 16:05:29 -0500

    Changing threads - fixing messages reply

--- server_threaded.py
+++ server_threaded.py
@@ -5,16 +5,18 @@ import threading
 import logging
 import yaml 
 import psycopg2
+import json 
+import time
 from datetime import datetime
 
-conf = yaml.load(open('application.yml'), Loader=yaml.BaseLoader)
-log_name = "log_threaded_server_{}".format(datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
-logging.basicConfig(filename=log_name)
+logging.basicConfig(filename='developer_info.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
 
+conf = yaml.load(open('application.yml'), Loader=yaml.BaseLoader)
 parser = argparse.ArgumentParser(description = "This is the server for the multithreaded socket.")
 parser.add_argument('--host', metavar = 'host', type = str, nargs = '?', default = socket.gethostname())
 parser.add_argument('--port', metavar = 'port', type = int, nargs = '?', default = 8443)
 args = parser.parse_args()
+
 
 print("Running the server on: {} and port: {}".format(args.host, args.port))
 
@@ -33,26 +35,37 @@ def get_vehicles():
 
 try: 
 	sck.bind((args.host, args.port))
-	sck.listen(5)
+	sck.listen()
 	#records = get_vehicles()
 except Exception as e:
 	raise SystemExit("We could not bind the server on host: {} to port: {}, because: {}".format(args.host, args.port, e))
 
-def on_new_client(client, connection):	
+def on_new_client(client, connection):
 	ip = connection[0]
 	port = connection[1]
+	logging.info("The new connection was made from IP: {}, and port: {}!".format(ip, port))
 	while True:
 		msg = client.recv(1024)
-		print("The client said: {}".format(msg.decode()))
-		handle_message(client, ip, port, msg)
-	logging.info("The client from ip: {}, and port: {}, has gracefully disconnected!".format(ip, port))
+		handle_message(client, msg)
 	client.close()
+	logging.info("The client from ip: {}, and port: {}, has gracefully disconnected!".format(ip, port))
 
-def handle_message(client, ip, port, messasge):
-	logging.info("The new connection was made from IP: {}, and port: {}!".format(ip, port))
-	logging.debug(messasge.decode())
-	reply = "OK"
-	client.send(reply.encode('utf-8'))
+def handle_message(client, messasge):
+	msg = messasge.decode()
+	logging.debug(msg)
+	loaded_json = json.loads(msg)
+	operation = loaded_json['OPERATION']
+	session_id = loaded_json['SESSION']
+	if operation == "CONNECT":
+		#device_id = loaded_json['PARAMETER']['DSNO']
+		reply = json.dumps({"MODULE":"CERTIFICATE","OPERATION":"CONNECT","RESPONSE":{"DEVTYPE":1,"ERRORCAUSE":"","ERRORCODE":0,"MASKCMD":1,"PRO":"1.0.4","VCODE":""},"SESSION":session_id})
+		client.send(reply.encode('utf-8'))
+		time.sleep(0.5)
+		set_binary = json.dumps({"MODULE":"CONFIGMODEL","OPERATION":"SET","PARAMETER":{"MDVR":{"KEYS":{"GV":1},"PGDSM":{"PGPS":{"EN":1}},"PIS":{"PC041245T":{"GU":{"EN":1,"IT":5}}},"PSI":{"CG":{"UEM":0}}}},"SESSION":session_id})
+		client.send(set_binary.encode('utf-8'))
+	elif operation == "KEEPALIVE":
+		reply = json.dumps({"MODULE":"CERTIFICATE","OPERATION":"KEEPALIVE","SESSION":session_id})
+		client.send(reply.encode('utf-8'))
 
 while True:
 	try: 
