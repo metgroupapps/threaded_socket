67adc326fd54dee430023d48bfe89a4e5668dd9f
aaochoa <oe.anderson@cerezostudio.com>
2020-03-31 11:35:38 -0500

    New test message

--- client_threaded.py
+++ client_threaded.py
@@ -16,8 +16,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
 		raise SystemExit(f"We have failed to connect to host: {args.host} on port: {args.port}, because: {e}")
 
 	while True:
-		msg = b'\x08\x00\x00\x00\x00\x00\x01\xf4R\x00\x00\x00{"MODULE":"CERTIFICATE","OPERATION":"CONNECT","PARAMETER":{"AUTOCAR":"prueba","AUTONO":"0","CARNUM":"","CHANNEL":16,"CID":10,"CNAME":"CUSTOMER_04.1519","DEVCLASS":4,"DEVNAME":"MDVR","DEVTYPE":1,"DLIP":[{"LIP":"0.0.0.0","MT":"eth0"}],"DLP":[80,9006],"DSNO":"00980001E4","EID":"APSI18_80","EV":"V1.1","FSV":0,"LINENO":"","MAC":[{"IMAC":"00:00:00:00:00:00","MT":"eth0"}],"MODE":1,"MTYPE":79,"NET":2,"PRO":"1.0.5","STYPE":74,"TSE":1,"UNAME":"","UNO":""},"SESSION":"b5b62487-2b98-4b00-bdf5-a05c3cd3a4ed"}\n'
-		sck.sendall(msg.encode('utf-8'))
+		msg = b'\x08\x00\x00\x00\x00\x00\x01\xfbR\x00\x00\x00{"MODULE":"CERTIFICATE","OPERATION":"CONNECT","PARAMETER":{"AUTOCAR":"prueba","AUTONO":"0","CARNUM":"","CHANNEL":16,"CID":572537376,"CNAME":"CUSTOMER_04.1519","DEVCLASS":4,"DEVNAME":"MDVR","DEVTYPE":1,"DLIP":[{"LIP":"0.0.0.0","MT":"eth0"}],"DLP":[80,9006],"DSNO":"00980001E4","EID":"APSI18_80","EV":"V1.1","FSV":0,"LINENO":"","MAC":[{"IMAC":"00:00:00:00:00:00","MT":"eth0"}],"MODE":1,"MTYPE":79,"NET":2,"PRO":"1.0.5","STYPE":74,"TSE":1,"UNAME":"","UNO":""},"SESSION":"185a10f3-c84b-45f4-947e-d24dab8728ba"}\n'
+		sck.sendall(msg)
 		if msg =='exit':
 			print("Client is saying goodbye!")
 			break
