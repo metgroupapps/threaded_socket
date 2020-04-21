030bc581b2f4a5c77b78bc699c050fad0d560f39
aaochoa <oe.anderson@cerezostudio.com>
2020-04-20 19:28:12 -0500

    Adding twisted module

--- twisted_example.py
+++ twisted_example.py
@@ -26,28 +26,27 @@ class TCPServer(protocol.Protocol):
       loaded_json = json.loads(tmp)
       operation = loaded_json['OPERATION']
       session_id = loaded_json['SESSION']
-      if operation == "CONNECT":
-        #device_id = loaded_json['PARAMETER']['DSNO']
-        self.connectionReply(session_id, operation)
-      elif operation == "KEEPALIVE":
-        reply = json.dumps({"MODULE":"CERTIFICATE","OPERATION":"KEEPALIVE","SESSION":session_id})    
+      self.connectionReply(session_id, operation)
     except Exception as e:
       logging.error("Failed fam!: {}".format(e))
       print("Failed fam!: {}".format(e))
     
   def connectionReply(self, session_id, operation):
-    payloadJson = json.dumps({"MODULE":"CERTIFICATE","OPERATION":"CONNECT","RESPONSE":{"DEVTYPE":1,"ERRORCAUSE":"","ERRORCODE":0,"MASKCMD":1,"PRO":"1.0.4","VCODE":""},"SESSION":session_id})
+    if operation == "CONNECT":
+      payloadJson = json.dumps({"MODULE":"CERTIFICATE","OPERATION":"CONNECT","RESPONSE":{"DEVTYPE":1,"ERRORCAUSE":"","ERRORCODE":0,"MASKCMD":1,"PRO":"1.0.4","VCODE":""},"SESSION":session_id})
+      #payloadJsonBinary = json.dumps({"MODULE":"CONFIGMODEL","OPERATION":"SET","PARAMETER":{"MDVR":{"KEYS":{"GV":1},"PGDSM":{"PGPS":{"EN":1}},"PIS":{"PC041245T":{"GU":{"EN":1,"IT":5}}},"PSI":{"CG":{"UEM":0}}}},"SESSION":session_id})
+      self.connectionMessage(payloadJson)
+      #self.connectionMessage(payloadJsonBinary)
+    elif operation == "KEEPALIVE":
+      reply = json.dumps({"MODULE":"CERTIFICATE","OPERATION":"KEEPALIVE","SESSION":session_id})  
+      self.connectionMessage(reply)
+
+  def connectionMessage(self, payloadJson):
     payload = str(payloadJson).replace(" ", "")
     pLength = sys.getsizeof(payload)
     midPart = pack('>i', pLength)
     completeMessage = FIRST_PART + midPart + END_PART + payload.encode('utf-8')
     self.transport.write(completeMessage)
-    #payloadJsonBinary = json.dumps({"MODULE":"CONFIGMODEL","OPERATION":"SET","PARAMETER":{"MDVR":{"KEYS":{"GV":1},"PGDSM":{"PGPS":{"EN":1}},"PIS":{"PC041245T":{"GU":{"EN":1,"IT":5}}},"PSI":{"CG":{"UEM":0}}}},"SESSION":session_id})
-    #payloadBinary = str(payloadJsonBinary).replace(" ", "")
-    #pLengthBinary = sys.getsizeof(payloadBinary)
-    #midPartBinary = pack('>i', pLengthBinary)
-    #completeMessageBinary = FIRST_PART +  midPartBinary + END_PART
-    #client.send(completeMessageBinary.encode('utf-8'))  
 
 
 def main():
