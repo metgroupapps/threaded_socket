08ec91f2f2f077051af466885e8bf4265495d229
aaochoa <oe.anderson@cerezostudio.com>
2020-05-19 16:26:00 -0500

    Changing test app

--- client_threaded.py
+++ client_threaded.py
@@ -26,24 +26,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
 		sck.sendall(msg2)
 		print(2)
 		time.sleep(1)
-		#msg3 = b'\x08\x00\x00\x00\x00\x00\x00\x00R\x00\x00\x00'
-		#sck.sendall(msg3)
-		#print(3)
-		#time.sleep(1)
-		#msg4 = b'\x16\x03\x01\x00\x8c\x01\x00\x00\x88\x03\x03\x17\x9a\xc9\x15+\x1c\xccsp\x9f\x88f\x0f\xbb\xd0\xf1\x93\x91\x0c\xc6\xd8\x11>\x0b4&\xdbG\x14ZOb\x00\x00\x1a\xc0/\xc0+\xc0\x11\xc0\x07\xc0\x13\xc0\t\xc0\x14\xc0\n\x00\x05\x00/\x005\xc0\x12\x00\n\x01\x00\x00E\x00\x00\x00\x13\x00\x11\x00\x00\x0e67.231.254.162\x00\x05\x00\x05\x01\x00\x00\x00\x00\x00\n\x00\x08\x00\x06\x00\x17\x00\x18\x00\x19\x00\x0b\x00\x02\x01\x00\x00\r\x00\n\x00\x08\x04\x01\x04\x03\x02\x01\x02\x03\xff\x01\x00\x01\x00'
-		#sck.sendall(msg4)
-		#print(4)
-		#time.sleep(1)
-		#msg5 = b'\x08\x00\x00\x00\x00\x00\x02nR\x00\x00\x00{"MODULE":"DEVEMM","OPERATION":"SPI","PARAMETER":{"M":2,"REAL":0,"S":{"ALARM":0,"G3":3,"G3S":3,"G4":0,"G4S":0,"RE":[2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0],"S":0,"SINFO":[{"DS":0,"LS":0,"O":0,"S":0,"T":0,"TS":2000330686464},{"DS":0,"LS":2457862144,"O":0,"S":0,"T":0,"TS":2000330686464}],"STC":2,"SU":0,"SW":0,"T":"20200508010216","TD":4000,"TM":"81288.035000","TRAFFIC":[{"I":"","RX":0,"T":0,"TS":0,"TX":0},{"I":"","RX":0,"T":1,"TS":0,"TX":0},{"I":"","RX":0,"T":2,"TS":0,"TX":0},{"I":"","RX":0,"T":3,"TS":0,"TX":0}],"V":2460,"VS":[0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1],"W":1,"WS":0}},"SESSION":"ccd944ec-289e-422c-ba25-36e9d7f31483"}\n'
-		#sck.sendall(msg5)
-		#print(5)
-		#time.sleep(1)
+		msg3 = b'\x08\x00\x00\x00\x00\x00\x00\x00R\x00\x00\x00'
+		sck.sendall(msg3)
+		print(3)
+		time.sleep(1)
+		msg4 = b'\x16\x03\x01\x00\x8c\x01\x00\x00\x88\x03\x03\x17\x9a\xc9\x15+\x1c\xccsp\x9f\x88f\x0f\xbb\xd0\xf1\x93\x91\x0c\xc6\xd8\x11>\x0b4&\xdbG\x14ZOb\x00\x00\x1a\xc0/\xc0+\xc0\x11\xc0\x07\xc0\x13\xc0\t\xc0\x14\xc0\n\x00\x05\x00/\x005\xc0\x12\x00\n\x01\x00\x00E\x00\x00\x00\x13\x00\x11\x00\x00\x0e67.231.254.162\x00\x05\x00\x05\x01\x00\x00\x00\x00\x00\n\x00\x08\x00\x06\x00\x17\x00\x18\x00\x19\x00\x0b\x00\x02\x01\x00\x00\r\x00\n\x00\x08\x04\x01\x04\x03\x02\x01\x02\x03\xff\x01\x00\x01\x00'
+		sck.sendall(msg4)
+		print(4)
+		time.sleep(1)
+		msg5 = b'\x08\x00\x00\x00\x00\x00\x02nR\x00\x00\x00{"MODULE":"DEVEMM","OPERATION":"SPI","PARAMETER":{"M":2,"REAL":0,"S":{"ALARM":0,"G3":3,"G3S":3,"G4":0,"G4S":0,"RE":[2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0],"S":0,"SINFO":[{"DS":0,"LS":0,"O":0,"S":0,"T":0,"TS":2000330686464},{"DS":0,"LS":2457862144,"O":0,"S":0,"T":0,"TS":2000330686464}],"STC":2,"SU":0,"SW":0,"T":"20200508010216","TD":4000,"TM":"81288.035000","TRAFFIC":[{"I":"","RX":0,"T":0,"TS":0,"TX":0},{"I":"","RX":0,"T":1,"TS":0,"TX":0},{"I":"","RX":0,"T":2,"TS":0,"TX":0},{"I":"","RX":0,"T":3,"TS":0,"TX":0}],"V":2460,"VS":[0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1],"W":1,"WS":0}},"SESSION":"ccd944ec-289e-422c-ba25-36e9d7f31483"}\n'
+		sck.sendall(msg5)
+		print(5)
+		time.sleep(1)
 		msg6 = b'\x08\x00\x00\x00\x00\x00\x00\x90R\x00\x00\x00{"MODULE":"DEVEMM","OPERATION":"SPI","PARAMETER":{"M":1,"P":{"C":0,"J":"-74.109406","S":0,"T":"20200508010216","V":0,"W":"4.708176"},"REAL":0}}\n'
 		sck.sendall(msg6)
-		print(6)
-		if msg2 =='exit':
-			print("Client is saying goodbye!")
-			break
+		print(6)	
 		data = sck.recv(1024)
 		print("The server's response was: {}".format(data))
 		time.sleep(15)
+		break
